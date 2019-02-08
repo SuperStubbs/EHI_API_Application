f73698e56128436bb97b336d29f3bbcfe1bc65f1
SuperStubbs <astubbl@gmail.com>
2019-02-07 22:09:56 -0600

    initial commit

---
+++ app/src/main/java/ehi/ehiapplication/ui/main/MainFragment.java
@@ -0,0 +1,49 @@
+package ehi.ehiapplication.ui.main;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import ehi.ehiapplication.R;
+
+public class MainFragment extends Fragment {
+
+    @BindView(R.id.message)
+    TextView message;
+
+    private MainViewModel mViewModel;
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        message.setText(getString(R.string.app_description));
+
+        return inflater.inflate(R.layout.main_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+    @OnClick(R.id.viewReposBtn)
+    protected void onViewReposClick() {
+
+    }
+}
