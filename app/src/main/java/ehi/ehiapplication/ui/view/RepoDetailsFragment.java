d02b452524e58e930262343e4d67d58ee6415ef3
SuperStubbs <astubbl@gmail.com>
2019-02-11 16:28:38 -0600

    Completed DetailRepoView. Need to fix homepage

--- app/src/main/java/ehi/ehiapplication/ui/view/RepoDetailsFragment.java
+++ app/src/main/java/ehi/ehiapplication/ui/view/RepoDetailsFragment.java
@@ -1,6 +1,5 @@
 package ehi.ehiapplication.ui.view;
 
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
@@ -12,19 +11,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.List;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import ehi.ehiapplication.R;
 import ehi.ehiapplication.databinding.RepoDetailBinding;
-import ehi.ehiapplication.models.Repo;
 import ehi.ehiapplication.viewmodels.RepoViewModel;
 
 public class RepoDetailsFragment extends Fragment {
-
-    @BindView(R.id.textView)
-    TextView tv;
 
     RepoDetailBinding binding;
     RepoViewModel repoViewModel;
@@ -38,14 +31,6 @@ public class RepoDetailsFragment extends Fragment {
         super.onCreate(bundle);
 
         repoViewModel = ViewModelProviders.of(getActivity()).get(RepoViewModel.class);
-
-        final Observer<Repo> repoObserver = newRepo -> {
-            if(newRepo != null) {
-                tv.setText(repoViewModel.getLastUpdatedDate());
-            }
-        };
-
-        repoViewModel.getRepo().observe(this, repoObserver);
     }
 
     @Nullable
